9b0e31747446178f15044b71a383315c62d5cd5a
Ben Wells <benwellsmusic@gmail.com>
2013-11-24 22:36:57 -0500

    index view template, server, and routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-var reminders = angular.module('reminders', []);
+var reminders = angular.module('reminders', ['ngRoute']);
+
+reminders.config(['$routeProvider','$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '../views/list.html',
+        controller: 'ViewRemindersCtrl',
+        caseInsensitiveMatch: true
+      }).otherwise({redirectTo:'/'});
+  }]);
--- js/controllers/ViewRemindersCtrl.js
+++ js/controllers/ViewRemindersCtrl.js
@@ -1,4 +1,15 @@
 reminders.controller('ViewRemindersCtrl', ['$scope', function ($scope){
+  $scope.reminders = [{
+    text: 'I need to do this',
+    completed: false
+  },
+  {
+    text: 'also this',
+    completed: true
+  }];
 
+  $scope.addReminder = function(){
+    
+  }
 }])
 
